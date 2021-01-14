7bbc2c19794954a60f071417265af2ed54c209ac
Narasimha Rao <tnrao.trainer@gmail.com>
2021-01-14 15:42:34 +0530

    commit message

--- WebApiValuesService/Controllers/ValuesController.cs
+++ WebApiValuesService/Controllers/ValuesController.cs
@@ -16,7 +16,7 @@ namespace WebApiValuesService.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value11", "value21", "Vinu", "Hanee" };
+            return new string[] { "value11", "value21", "Vinu", "Hanee", "Hi", "Hello" };
         }
 
         // GET api/<ValuesController>/5
